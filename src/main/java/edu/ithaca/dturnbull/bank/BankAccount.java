17e89b69c409996637278cfca8bf99eb91e2e02e
tjones4261 <73116929+tjones4261@users.noreply.github.com>
2022-01-27 09:42:10 -0500

    Modified isEmailValid to pass tests

--- src/main/java/edu/ithaca/dturnbull/bank/BankAccount.java
+++ src/main/java/edu/ithaca/dturnbull/bank/BankAccount.java
@@ -40,11 +40,36 @@ public class BankAccount {
 
 
     public static boolean isEmailValid(String email){
-        if (email.indexOf('@') == -1){
+        //if it does not contain an @ or has no prefix
+        if (email.indexOf('@') == -1 || email.indexOf('@') == 0){
             return false;
         }
         else {
-            return true;
+            //if there is more than 1 @
+            int count = 0;
+            for(int i = 0; i<email.length(); i++){
+                if(email.charAt(i) == '@'){
+                    count++;
+                }
+            }
+
+            if(count > 1)
+                return false;
         }
+
+        //if the @ is after the period
+        if(email.lastIndexOf('@') > email.lastIndexOf('.')){
+            return false;
+        }
+        else{
+            //if suffix has less than 2 characters after the period  g@f.om
+            if(email.lastIndexOf('.') > email.length() - 3){
+                return false;
+            }
+        }
+
+        return true;
+
+        
     }
 }
